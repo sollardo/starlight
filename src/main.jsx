71910720b1a82127c712b04e92b17935e7188d6d
Thedogecraft <dantaealt@gmail.com>
2025-06-28 20:39:24 -0700

    add posthog

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,25 @@ import { RecoilRoot } from "recoil";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import initLocalStorage from "./lib/init.js";
+import { PostHogProvider } from 'posthog-js/react';
 
 initLocalStorage();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RecoilRoot>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </RecoilRoot>
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        capture_exceptions: true,
+        debug: import.meta.env.MODE === "development",
+      }}
+    >
+      <RecoilRoot>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </RecoilRoot>
+    </PostHogProvider>
   </StrictMode>
 );
